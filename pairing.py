219f7db874b6714e0069c9be049af6af2f7503fb
mwguthrie <stampeder@gmail.com>
2019-11-20 15:29:27 -0500

    Update pairing.py

--- pairing.py
+++ pairing.py
@@ -56,23 +56,23 @@ def main():
             if rand_vec[ind] == ind:
                 conditions_met = False
                 break
-            # Condition 2: not to their own Secret Valentine
+            # Condition 2: not to their own Secret Santa
             elif rand_vec[rand_vec[ind]] == ind:
                 conditions_met = False
                 break
-            # Condition 3: not to 'roommates' (people with same room number listed)
+            # Condition 3: not to partners
             elif givers_list[ind][2] == givers_list[rand_vec[ind]][2]:
                 conditions_met = False
                 break
-            # Condition 4: not to person X if your roommate has person X's roommate
-            roommates = [i for i,g in enumerate(givers_list) if g[2] == givers_list[ind][2] and i is not ind] # people with same room number
-            # for roommates of 'ind', check that they do not give to the same 'team'
-            for roommate in roommates:
-                # If A1 -> B1, then NOT A2 -> B2
-                # i.e. check if recepient of ind is not from same room as recipient of roommate
-                if givers_list[rand_vec[ind]][2] == givers_list[rand_vec[roommate]][2]: 
-                    conditions_met = False
-                    break
+#            # Condition 4: not to person X if your partner has person X's partner (this condition isn't necessary for us)
+#            partners = [i for i,g in enumerate(givers_list) if g[2] == givers_list[ind][2] and i is not ind] # people with same room number
+#            # for roommates of 'ind', check that they do not give to the same 'team'
+#            for partner in partners:
+#                # If A1 -> B1, then NOT A2 -> B2
+#                # i.e. check if recepient of ind is not from same room as recipient of roommate
+#                if givers_list[rand_vec[ind]][2] == givers_list[rand_vec[partner]][2]: 
+#                    conditions_met = False
+#                    break
     write_file(givers_list, rand_vec) # write csv file with pairs
     for ind in range(N): # print in Terminal
         print givers_list[ind][1], "-->", \
